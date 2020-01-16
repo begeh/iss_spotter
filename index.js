adf540d8ac06c00f220c92bb676154659fa48268
Bashir Egeh <b_egeh@hotmail.com>
2020-01-16 23:47:50 +0000

    added third ISS function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -20,3 +21,11 @@ fetchCoordsByIP('66.207.199.230', (error, data) => {
   coord.longitude = data.longitude;
   console.log(coord);
 });
+
+fetchISSFlyOverTimes({ latitude: '43.644098', longitude: '-79.402316' }, (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned flyover times:' , data);
+})
--- iss.js
+++ iss.js
@@ -22,12 +22,12 @@ const fetchMyIP = function (callback) {
 };
 
 const fetchCoordsByIP = function (ip, callback) {
-  request(`https://ipvigilante.com/${ip}`, (error, response, body)=>{
-    if (error){
-      callback(error, null); 
+  request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
       return;
     }
-    if(response.statusCode !== 200){
+    if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCode} when fetching coordinates for IP: ${body}`, null));
       return;
     }
@@ -36,7 +36,34 @@ const fetchCoordsByIP = function (ip, callback) {
   });
 };
 
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function (coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`, null));
+      return;
+    }
+    let data = JSON.parse(body).response;
+    callback(null, data);
+  });
+
+};
+
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
 };
